524765d4290936644d405adbebc4c5ede5a7fe95
piyushmallick001 <piyushmallick001@gmail.com>
2023-10-28 19:40:54 +0530

    added ios

--- campus_connect/ios/Runner/AppDelegate.swift
+++ campus_connect/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    // TODO: Add your Google Maps API key
     GMSServices.provideAPIKey("AIzaSyDVR-Yn6k8qa-hTSNUBw-H5sL5sLCiLux4")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
